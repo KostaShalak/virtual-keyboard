80977aeb40fc1576ef366efb0e3da88df7544a58
Kosta Shalak <zapalka9991@mail.ru>
2023-05-01 23:26:42 +0300

    fix: remove keyboard js func

--- main.js
+++ main.js
@@ -107,11 +107,3 @@ document.addEventListener('keyup', function(e) {
         }
     }
 });
-
-keyboard.addEventListener('click', function(e) {
-    for(let i = 0; i < keys.length; i++) {
-        if(e.key == keys[i].getAttribute('data-key' ) || e.key == keys[i].getAttribute('lowerCaseName')) {
-            keys[i].classList.toggle('active')
-        }
-    }
-});
